2f5a2741229445e75f408e7b6487d32ea94dcb91
Magich <75262956+presidentofgoblin@users.noreply.github.com>
2021-02-27 17:21:36 +0800

    Add files via upload

---
+++ Ultrapowa_Clash_Server/GameFiles/FingerPrint.cs
@@ -0,0 +1,22 @@
+﻿namespace Ultrapowa_Clash_Server.GameFiles
+{
+    using Newtonsoft.Json;
+    using System;
+    using System.Collections.Generic;
+    using System.Runtime.CompilerServices;
+
+    internal class FingerPrint
+    {
+        public string ToJson()
+        {
+            return JsonConvert.SerializeObject(this).Replace("/", @"\/");
+        }
+
+        public List<GameFile> files { get; set; }
+
+        public string sha { get; set; }
+
+        public string version { get; set; }
+    }
+}
+
---
+++ Ultrapowa_Clash_Server/GameFiles/GameFile.cs
@@ -0,0 +1,13 @@
+﻿namespace Ultrapowa_Clash_Server.GameFiles
+{
+    using System;
+    using System.Runtime.CompilerServices;
+
+    internal class GameFile
+    {
+        public string file { get; set; }
+
+        public string sha { get; set; }
+    }
+}
+
